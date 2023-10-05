8031236c5ceac42adb571984ba40c4afb010ee78
Tao Jin <tao-j@outlook.com>
2023-10-04 22:08:37 -0400

    move ble function into separate files

--- ble_tx_mock.py
+++ ble_tx_mock.py
@@ -1,298 +1,11 @@
-import asyncio
-import struct
+import random
 import time
+import asyncio
 
-from bluez_peripheral.gatt.service import Service, ServiceCollection
-from bluez_peripheral.gatt.characteristic import (
-    characteristic,
-    CharacteristicFlags as CharFlags,
-)
-from bluez_peripheral.util import *
-from bluez_peripheral.advert import Advertisement, AdvertisingIncludes
-from bluez_peripheral.agent import NoIoAgent
+from ble_tx import BLE_Tx
+from keiser import KeiserBLEListener
 
-# cycling speed and cadence
-CSC_UUID = "1816"
-CSC_MEASUREMENT_UUID = "2A5B"
-CSC_FEATURE_UUID = "2A5C"
-SC_CONTROL_POINT_UUID = "2A55"
-
-# cycling power
-CP_UUID = "1818"
-CP_MEASUREMENT_UUID = "2A63"
-CP_VECTOR_UUID = "2A64"
-CP_FEATURE_UUID = "2A65"
-CP_CONTROL_POINT_UUID = "2A66"
-
-SENSOR_LOCATION_UUID = "2A5D"
-SENSOR_LOCATION_CHAR_UUID = 0x2A5D
-SENSOR_LOCATION_OTHER = 0
-SENSOR_LOCATION_TOP_OF_SHOE = 1
-SENSOR_LOCATION_IN_SHOE = 2
-SENSOR_LOCATION_HIP = 3
-SENSOR_LOCATION_FRONT_WHEEL = 4
-SENSOR_LOCATION_LEFT_CRANK = 5
-SENSOR_LOCATION_RIGHT_CRANK = 6
-SENSOR_LOCATION_LEFT_PEDAL = 7
-SENSOR_LOCATION_RIGHT_PEDAL = 8
-SENSOR_LOCATION_FRONT_HUB = 9
-SENSOR_LOCATION_REAR_DROPOUT = 10
-SENSOR_LOCATION_CHAINSTAY = 11
-SENSOR_LOCATION_REAR_WHEEL = 12
-SENSOR_LOCATION_REAR_HUB = 13
-SENSOR_LOCATION_CHEST = 14
-SENSOR_LOCATION_SPIDER = 15
-SENSOR_LOCATION_CHAIN_RING = 16
-
-# device information
-DI_UUID = "180A"
-DI_SYSTEM_ID_UUID = "2A23"
-DI_MODEL_NUMBER_UUID = "2A24"
-DI_SERIAL_NUMBER_UUID = "2A25"
-DI_FIRMWARE_REVISION_UUID = "2A26"
-DI_HARDWARE_REVISION_UUID = "2A27"
-DI_SOFTWARE_REVISION_UUID = "2A28"
-DI_MANUFACTURER_NAME_UUID = "2A29"
-
-# battery service
-BAT_UUID = "180F"
-BAT_LEVEL_UUID = "2A19"
-
-CSC_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT = 0b0000_0001
-CSC_F_BIT_CRANK_REVOLUTION_DATA_PRESENT = 0b0000_0010
-
-# feature flags
-CP_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT = 0b0001_0000
-CP_F_BIT_CRANK_REVOLUTION_DATA_PRESENT = 0b0010_0000
-# measurement flags
-CP_M_BIT_WHEEL_REVOLUTION_DATA_PRESENT = 0b0000_0100
-CP_M_BIT_CRANK_REVOLUTION_DATA_PRESENT = 0b0000_1000
-
-
-class BatteryService(Service):
-    def __init__(self):
-        super().__init__(BAT_UUID, primary=True)
-
-    @characteristic(BAT_LEVEL_UUID, CharFlags.READ | CharFlags.NOTIFY)
-    def battery_level(self, options):
-        return struct.pack("<B", *[100])
-
-
-class DeviceInformationService(Service):
-    def __init__(self):
-        super().__init__(DI_UUID)
-
-    @characteristic(DI_SYSTEM_ID_UUID, CharFlags.READ)
-    def system_id(self, options):
-        return struct.pack("<Q", *[0x0000022001100000])
-
-    @characteristic(DI_MODEL_NUMBER_UUID, CharFlags.READ)
-    def model_number(self, options):
-        return bytearray(b"Keiser M to GATT")
-
-    @characteristic(DI_SERIAL_NUMBER_UUID, CharFlags.READ)
-    def serial_number(self, options):
-        return bytearray(b"12345678")
-
-    @characteristic(DI_FIRMWARE_REVISION_UUID, CharFlags.READ)
-    def firmware_revision(self, options):
-        return bytearray(b"0.0.1")
-
-    @characteristic(DI_HARDWARE_REVISION_UUID, CharFlags.READ)
-    def hardware_revision(self, options):
-        return bytearray(b"0.1.1")
-
-    @characteristic(DI_SOFTWARE_REVISION_UUID, CharFlags.READ)
-    def software_revision(self, options):
-        return bytearray(b"1.0beta")
-
-    @characteristic(DI_MANUFACTURER_NAME_UUID, CharFlags.READ)
-    def manufacturer_name(self, options):
-        return bytearray(b"t-j")
-
-
-class CPService(Service):
-    def __init__(self):
-        self.measure_flags = (
-            0x0
-            # | CP_M_BIT_CRANK_REVOLUTION_DATA_PRESENT
-            # | CP_M_BIT_WHEEL_REVOLUTION_DATA_PRESENT
-        )
-        self.feature_flags = (
-            0x0
-            # | CP_F_BIT_CRANK_REVOLUTION_DATA_PRESENT
-            # | CP_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT
-        )
-        super().__init__(CP_UUID, primary=True)
-
-    @characteristic(CP_MEASUREMENT_UUID, CharFlags.NOTIFY | CharFlags.READ)
-    def cp_measurement(self, options):
-        pass
-
-    def notify_new_rate(self, power, w_event_ms, c_event_ms, crank_rev, wheel_rev):
-        rate = struct.pack(
-            "<Hh",
-            *[
-                self.measure_flags,
-                power & 0x7FFF,
-                # wheel_rev & 0xFFFFFFFF,
-                # w_event_ms & 0xFFFF,
-                # crank_rev & 0xFFFF,
-                # c_event_ms & 0xFFFF,
-            ],
-        )
-        self.cp_measurement.changed(rate)
-
-    @characteristic(CP_FEATURE_UUID, CharFlags.READ)
-    def cp_feature(self, options):
-        return struct.pack("<I", *[self.feature_flags])
-
-    @characteristic(SENSOR_LOCATION_UUID, CharFlags.READ)
-    def sensor_location(self, options):
-        return struct.pack("<B", *[SENSOR_LOCATION_REAR_WHEEL])
-
-    @characteristic(CP_CONTROL_POINT_UUID, CharFlags.WRITE | CharFlags.INDICATE)
-    def cp_control_point(self, options):
-        pass
-
-
-class CSCService(Service):
-    def __init__(self):
-        super().__init__(CSC_UUID, True)
-        self.feature = (
-            0
-            | CSC_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT
-            | CSC_F_BIT_CRANK_REVOLUTION_DATA_PRESENT
-        )
-
-    @characteristic(CSC_MEASUREMENT_UUID, CharFlags.NOTIFY | CharFlags.READ)
-    def csc_measurement(self, options):
-        pass
-
-    # @csc_measurement.descriptor("2902")
-    # def csc_measurement_descriptor(self, options):
-    #     return struct.pack("<H", *[0x0001])
-
-    def notify_all(self, wheel_rev, crank_rev, w_event_ms, c_event_ms):
-        rate = struct.pack(
-            "<BIHHH",
-            CSC_F_BIT_CRANK_REVOLUTION_DATA_PRESENT
-            | CSC_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT,
-            wheel_rev & 0xFFFFFFFF,
-            w_event_ms & 0xFFFF,
-            crank_rev & 0xFFFF,
-            c_event_ms & 0xFFFF,
-        )
-        self.csc_measurement.changed(rate)
-
-    def notify_crank(self, wheel_rev, crank_rev, w_event_ms, c_event_ms):
-        rate = struct.pack(
-            "<BHH",
-            CSC_F_BIT_CRANK_REVOLUTION_DATA_PRESENT,
-            crank_rev & 0xFFFF,
-            c_event_ms & 0xFFFF,
-        )
-        self.csc_measurement.changed(rate)
-
-    def notify_wheel(self, wheel_rev, crank_rev, w_event_ms, c_event_ms):
-        rate = struct.pack(
-            "<BIH",
-            CSC_F_BIT_WHEEL_REVOLUTION_DATA_PRESENT,
-            wheel_rev & 0xFFFFFFFF,
-            w_event_ms & 0xFFFF,
-        )
-        self.csc_measurement.changed(rate)
-
-    @characteristic(CSC_FEATURE_UUID, CharFlags.READ)
-    def csc_feature(self, options):
-        return struct.pack("<H", *[self.feature])
-
-    @characteristic(SENSOR_LOCATION_UUID, CharFlags.READ)
-    def sensor_location(self, options):
-        return struct.pack("<B", *[SENSOR_LOCATION_OTHER])
-
-    @characteristic(SC_CONTROL_POINT_UUID, CharFlags.WRITE | CharFlags.INDICATE)
-    def sc_control_point(self, options):
-        pass
-
-
-async def main():
-    # system busmfrom dbus_next.
-    bus = await get_message_bus()
-
-    bat_service = BatteryService()
-    di_service = DeviceInformationService()
-    cp_service = CPService()
-    csc_service = CSCService()
-    svcs = ServiceCollection([bat_service, di_service, cp_service, csc_service])
-    await svcs.register(bus)
-
-    # An agent is required to handle pairing
-    # This script may need superuser for this to work.
-    agent = NoIoAgent()
-    await agent.register(bus)
-
-    adapter = await Adapter.get_first(bus)
-    print(await adapter.get_address())
-
-    # Start an advert that will last for 60 seconds.
-    advert = Advertisement(
-        localName="Keiser M to GATT",
-        serviceUUIDs=[BAT_UUID, DI_UUID, CP_UUID, CSC_UUID],
-        appearance=0x0480,
-        timeout=0,
-        includes=AdvertisingIncludes.TX_POWER,
-    )
-    await advert.register(bus, adapter)
-
-    import random
-
-    last_t = time.time()
-    wheel_rev_cls = ReverseRev()
-    crank_rev_cls = ReverseRev()
-    interval = 0.25
-    while True:
-        await asyncio.sleep(interval)
-        this_t = time.time() - last_t
-        power = random.randint(120, 133)
-
-        rev = interval * 1.1
-        wheel_rev_cls.add(rev * 8, this_t)
-        crank_rev_cls.add(rev * 4, this_t)
-
-        cr, cev = crank_rev_cls.get()
-        wr, wev = wheel_rev_cls.get()
-
-        print(wheel_rev_cls.get(), crank_rev_cls.get(), power)
-
-        if crank_rev_cls.notify and wheel_rev_cls.notify:
-            csc_service.notify_all(
-                wheel_rev=wr, crank_rev=cr, w_event_ms=wev, c_event_ms=cev
-            )
-            crank_rev_cls.notify = False
-            wheel_rev_cls.notify = False
-        # else:
-        #     if crank_rev_cls.notify:
-        #         csc_service.notify_crank(
-        #             wheel_rev=wr, crank_rev=cr, w_event_ms=wev, c_event_ms=cev
-        #         )
-        #     if wheel_rev_cls.notify:
-        #         csc_service.notify_wheel(
-        #             wheel_rev=wr, crank_rev=cr, w_event_ms=wev, c_event_ms=cev
-        #         )
-        cp_service.notify_new_rate(
-            power=power,
-            wheel_rev=wr,
-            crank_rev=cr,
-            w_event_ms=wev,
-            c_event_ms=cev,
-        )
-        # Handle dbus requests.
-
-    await bus.wait_for_disconnect()
-
-
-class ReverseRev:
+class MockDataRx:
     def __init__(self) -> None:
         self.rev_float = 0
         self.rev_event = 0
@@ -317,6 +30,60 @@ class ReverseRev:
 
     def get(self):
         return self.rev_event & 0xFFFF, int(self.event_time_ms) & 0xFFFF
+
+
+class BikeData:
+    def __init__(self) -> None:
+        self.cr = 0
+        self.cev = 0
+        self.wr = 0
+        self.wev = 0
+        self.power = 0
+
+    async def loop(self):
+        last_t = time.time()
+        wheel_rev_cls = MockDataRx()
+        crank_rev_cls = MockDataRx()
+        interval = 0.25
+        while True:
+            await asyncio.sleep(interval)
+            this_t = time.time() - last_t
+
+            rev = interval * 1.1
+            wheel_rev_cls.add(rev * 2, this_t)
+            crank_rev_cls.add(rev * 1, this_t)
+
+            self.cr, self.cev = crank_rev_cls.get()
+            self.wr, self.wev = wheel_rev_cls.get()
+            self.power = random.randint(120, 133)
+
+            print(wheel_rev_cls.get(), crank_rev_cls.get(), self.power)
+
+class BikeDataKS(BikeData):
+    def __init__(self, kl) -> None:
+        self.kl = kl
+        super().__init__()
+
+    # async def loop(self):
+    #     self.
+
+async def main():
+    ble_tx = BLE_Tx()
+    await ble_tx.setup()
+    kl = KeiserBLEListener(bike_id=15)
+    bike_data = BikeData()
+    # bike_data = BikeDataKS(kl)
+
+    keiser_rx_task = asyncio.create_task(kl.loop())
+    ble_tx_task = asyncio.create_task(ble_tx.loop(bike_data=bike_data))
+    bike_data_task = asyncio.create_task(bike_data.loop())
+
+    try:
+        await ble_tx_task
+        await bike_data_task
+        await keiser_rx_task
+    except asyncio.exceptions.CancelledError:
+        print("CancelledError")
 
 
 if __name__ == "__main__":
